458d588efa311f435848f45891745998a5a44c53
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2019-12-18 19:13:58 -0800

    added sites to onclick in js

--- script.js
+++ script.js
@@ -1,9 +1,18 @@
-var quiz = $("#quiz");
-var calendar = $("#calendar");
-var project = $("#project");
+var quiz = $(".quiz");
+var calendar = $(".calendar");
+var project = $(".project");
 
 quiz.click(function (e) {
     e.preventDefault();
-    var quizLink = $("<a>");
-    quizLink.attr("href", "https://equilinquin.github.io/Web-API/");
+    window.open("https://equilinquin.github.io/Web-API/");
+});
+
+calendar.click(function (e) {
+    e.preventDefault();
+    window.open("https://equilinquin.github.io/Day-Calender/");
+});
+
+project.click(function (e) {
+    e.preventDefault();
+    window.open("https://tcanbolat.github.io/Group-Repo/");
 });
