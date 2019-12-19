dc636c40132f446c3dc29301ce4bc8a71c2cef71
Elizabeth Garcia-Quilinquin <equilinquin@gmail.com>
2019-12-18 22:17:09 -0800

    added github, linkedin, email, phone

--- script.js
+++ script.js
@@ -1,18 +1,22 @@
-var quiz = $(".quiz");
-var calendar = $(".calendar");
-var project = $(".project");
-
-quiz.click(function (e) {
+$(".quiz").click(function (e) {
     e.preventDefault();
     window.open("https://equilinquin.github.io/Web-API/");
 });
 
-calendar.click(function (e) {
+$(".calendar").click(function (e) {
     e.preventDefault();
     window.open("https://equilinquin.github.io/Day-Calender/");
 });
 
-project.click(function (e) {
+$(".project").click(function (e) {
     e.preventDefault();
     window.open("https://tcanbolat.github.io/Group-Repo/");
 });
+$("#github").click(function(e) {
+    e.preventDefault();
+    window.open("https://github.com/equilinquin");
+})
+$("#linkedin").click(function(e) {
+    e.preventDefault();
+    window.open("https://www.linkedin.com/in/elizabeth-garcia-quilinquin-207834191/")
+})
